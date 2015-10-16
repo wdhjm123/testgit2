f99cd054589e7291caf88055e205e4913da7a3b8
红领巾 <355693381@qq.com>
2015-10-16 16:17:53 +0800

    测试

--- app/src/main/java/com/example/test/myapplication/MainActivity.java
+++ app/src/main/java/com/example/test/myapplication/MainActivity.java
@@ -11,7 +11,8 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        //测试
     }
 
     @Override
